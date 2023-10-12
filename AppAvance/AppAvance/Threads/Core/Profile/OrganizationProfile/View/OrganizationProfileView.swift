2ce373c83389fcd58268998fa8f8b6e30c8482fb
Patricio <patovw@gmail.com>
2023-10-11 18:10:14 -0600

    update

--- AppAvance/AppAvance/Threads/Core/Profile/OrganizationProfile/View/OrganizationProfileView.swift
+++ AppAvance/AppAvance/Threads/Core/Profile/OrganizationProfile/View/OrganizationProfileView.swift
@@ -16,17 +16,17 @@ struct OrganizationProfileView: View {
     @State var starClicked = false
     @State private var imageLoadSuccess: Bool? = nil
     @State var bannerRetrieved = true
-    
+
     let url = URL(string: "https://www.instagram.com/")!
-    
+
     init(organization: Organization) {
         self._viewModel = StateObject(wrappedValue: OrganizationProfileViewModel(organization: organization))
     }
-    
+
     private var organization: Organization {
         return viewModel.organization
     }
-    
+
     var body: some View {
         GeometryReader { geometry in
             ScrollView(showsIndicators: false) {
@@ -55,35 +55,35 @@ struct OrganizationProfileView: View {
                             }
                         )
                         .edgesIgnoringSafeArea(.all)
-                        
+
                         VStack {
                             Rectangle()
                                 .fill(Color.white.opacity(0.8))
                                 .edgesIgnoringSafeArea(.all)
                         }
-                        
+
                         // Logo, Title, Description, and Buttons
                         VStack(spacing: 8) {
                             Spacer(minLength: 20) // Space to avoid overlapping with navigation bar
-                            
+
                             HStack {
                                 VStack(alignment: .leading) {
                                     Text(organization.name)
                                         .font(.title)
                                         .fontWeight(.semibold)
-                                    
+
                                     if let desc = organization.description {
                                         Text(desc)
                                     }
                                 }
-                                
+
                                 Spacer()
-                                
+
                                 OrganizationCircularLogoImageView(logoUrl: organization.logoUrl, size: .large)
                             }
-                            
+
                             Spacer()
-                            
+
                             // Favorite and Share buttons
                             HStack(spacing: 10) {
                                 // Sharing button
@@ -121,7 +121,7 @@ struct OrganizationProfileView: View {
                         .padding([.leading, .top, .bottom, .trailing])
                     }
                     .frame(height: geometry.size.height / 4)
-                    
+
                     Spacer()
 
                     VStack {
@@ -145,7 +145,7 @@ struct OrganizationProfileView: View {
                         .background(Color.white.opacity(0.8))
                         .cornerRadius(12)
                         .shadow(radius: 5)
-                        
+
                         // Social Networks
                         HStack(spacing: 10) {
                             ForEach(organization.socialNetworks, id: \.name) { network in
@@ -154,7 +154,7 @@ struct OrganizationProfileView: View {
                                 }
                             }
                         }
-                        
+
                         // Schedule
                         if let schedule = organization.schedule {
                             VStack(alignment: .leading, spacing: 8) {
@@ -180,7 +180,7 @@ struct OrganizationProfileView: View {
             }
         }
     }
-    
+
     func getBannerHeight(geometry: GeometryProxy) -> CGFloat {
         let offset = geometry.frame(in: .global).minY
         let height: CGFloat = geometry.size.height / 4
