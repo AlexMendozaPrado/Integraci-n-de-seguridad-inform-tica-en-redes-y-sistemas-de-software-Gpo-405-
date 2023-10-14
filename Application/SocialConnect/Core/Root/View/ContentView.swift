57f95ab73d55b58c129191a600f7d7c5133b87a6
Patricio <patovw@gmail.com>
2023-10-14 00:06:21 -0600

    Changed gitignore

--- Application/SocialConnect/Core/Root/View/ContentView.swift
+++ Application/SocialConnect/Core/Root/View/ContentView.swift
@@ -4,6 +4,7 @@ import JWTDecode
 struct ContentView: View {
     @StateObject var viewModel = ContentViewModel()
     
+    
     var body: some View {
         Group {
             if !viewModel.isValidJWT() {
